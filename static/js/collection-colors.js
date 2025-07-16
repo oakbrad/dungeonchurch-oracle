974ce720473da0283501462f15a2b826e9c673af
GitHub Action <action@github.com>
2025-07-16 11:36:59 +0000

    Update graph data and collection colors

--- static/js/collection-colors.js
+++ static/js/collection-colors.js
@@ -9,11 +9,11 @@ const collectionColors = {
   "7275a3d8-27da-4f63-ac39-a9bc9a1ec6d7": "#ffbe0b",
   "184c6e39-ef59-49b4-89d8-302d6abae3cf": "#ff4dfa",
   "28015ad0-50f5-4495-a6c5-2415436a3d40": "#ff825c",
+  "ac7efc59-549b-4aa9-9ed8-1b22f82382d0": "#FF4DFA",
   "7c70ca02-fdd6-4a30-a8b7-931093ebfbd8": "#ff5c80",
   "9870bc72-55da-4158-892c-3c54ec9e5828": null,
   "098323c3-c9a2-45f4-ab12-ff8b759c5be7": "#9e5cf7",
-  "77c74010-e20c-419c-8122-97563ec685b6": "#00d084",
-  "ac7efc59-549b-4aa9-9ed8-1b22f82382d0": "#FF4DFA"
+  "77c74010-e20c-419c-8122-97563ec685b6": "#00d084"
 };
 
 // Function to get color for a collection
